43a08b74e6d00fbe80a56b99996d2265cd02fedf
sanjuGuna <sanjay27it@gmail.com>
2026-03-04 18:09:20 +0530

    task group in airlflow

---
+++ dags/taskgrp.py
@@ -0,0 +1,29 @@
+from airflow.sdk import dag, task
+from datetime import datetime
+from airflow.utils.task_group import TaskGroup
+@dag(
+    schedule=None,
+    start_date=datetime(2026,3,3),
+    tags=["task_group"]
+)
+
+def taskgrp():#grouping of similar tasks to organise and strucuting
+    with TaskGroup("Extracting_group") as extract:
+        @task
+        def extract_user():
+            print("Extracting users")
+    
+        @task
+        def extract_user_playlist():
+            print("extracting user playlist")
+        
+        extract_user()
+        extract_user_playlist()
+    
+    @task
+    def transform():
+        print("Transfromed")
+    
+    extract >> transform()
+
+dag=taskgrp()
